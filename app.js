06fb0b91f27c4955fb960a6d424a3d959fde17c7
Ruby Tsai <hjtsai0307@gmail.com>
2021-07-06 15:18:16 +0800

    Add mongoose and Todo model.

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 // set routes
 app.get('/', (req, res) => {
