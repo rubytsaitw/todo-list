7f5e628134a472fed6d522affd93af2519b9c6db
Ruby Tsai <hjtsai0307@gmail.com>
2021-08-04 13:24:57 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -31,5 +31,10 @@ router.post('/register', (req, res) => {
       }
     })
 })
+ 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 
 module.exports = router
